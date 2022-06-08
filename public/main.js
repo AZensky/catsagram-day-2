18de16ffcb1067eaca00121d3ff31cbc431d0d99
RayC206 <rcv.henry@gmail.com>
2022-06-08 12:55:00 -0700

    catsagram phase 2

--- public/main.js
+++ public/main.js
@@ -31,6 +31,28 @@ export const createMainContent = () => {
   downvoteBtn.innerHTML = "downvote";
   upvoteBtn.style.margin = "5px";
 
+  //Comment input
+
+  const commentContainer = document.createElement("div");
+  const commentLabel = document.createElement("span");
+  const inputBox = document.createElement("input");
+  const submitBtn = document.createElement("button");
+
+  commentLabel.innerHTML = "Comment";
+  submitBtn.innerHTML = "Submit";
+
+  //Comment Section
+
+  const commentSection = document.createElement("div");
+  commentSection.style.border = "1px solid black"
+  commentSection.style.width = "600px";
+  commentSection.style.height = "600px";
+  const comments = document.createElement("ul");
+
+
+
+
+
   const container = document.querySelector(".container");
   container.appendChild(h1);
   container.appendChild(img);
@@ -41,11 +63,26 @@ export const createMainContent = () => {
   popBtnContainer.appendChild(upvoteBtn);
   popBtnContainer.appendChild(downvoteBtn);
 
+  //comment box
+
+  container.appendChild(commentContainer);
+  commentContainer.appendChild(commentLabel);
+  commentContainer.appendChild(inputBox);
+  commentContainer.appendChild(submitBtn);
+
+  //comment section
+  container.appendChild(commentSection);
+  commentSection.appendChild(comments);
+
   newImgBtn.addEventListener("click", fetchImage);
 
   upvoteBtn.addEventListener("click", upvote);
 
   downvoteBtn.addEventListener("click", downvote);
+
+  submitBtn.addEventListener("click", makeComment);
+
+
 
   function upvote() {
     popScore.innerHTML++;
@@ -53,6 +90,14 @@ export const createMainContent = () => {
 
   function downvote() {
     popScore.innerHTML--;
+  }
+
+  function makeComment() {
+    const val = inputBox.value;
+    const newComment = document.createElement("li");
+    newComment.innerText = val;
+    inputBox.value = "";
+    comments.appendChild(newComment);
   }
 
   fetchImage();
